3ec3874b288145295c3088dda2e68602aa39faa2
Rafael Rotiroti <rotiroti@gmail.com>
2021-10-19 23:34:56 -0300

    cors added

--- src/app.py
+++ src/app.py
@@ -1,11 +1,29 @@
 from typing import Dict
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.schemas import MultipleLuckyBetsResponse
 from src.core import multiple_luckynumbers
 
+origins = [
+    "http://localhost.com",
+    "https://localhost.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,4 @@
-from typing import Dict, List
-from typing import List, Dict
+from typing import List
 
 from pydantic import BaseModel
 
